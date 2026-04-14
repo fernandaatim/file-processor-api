4f595ebd219b06dd119fd506d1a45f653311190b
fernandaatim <nandaatin2012@gmail.com>
2026-04-14 11:47:08 -0300

    feat: adicionando autenticação via API Key

--- src/app/routes/upload.py
+++ src/app/routes/upload.py
@@ -1,14 +1,21 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi.security.api_key import APIKeyHeader
 from app.services.processor import process_file
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 MAX_FILE_SIZE = int(config.get("MAX_FILE_SIZE", 5000000))
+API_KEY = config.get("API_KEY",)
+
+api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
 
 router = APIRouter()
 
 @router.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(file: UploadFile = File(...), api_key: str = api_key_header):
+    if api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Chave de API inválida.")
+
     contents = await file.read()
     if len(contents) > MAX_FILE_SIZE:
         raise HTTPException(status_code=400, detail="Arquivo excede o tamanho máximo permitido.")
